073a1278a152c565ada7e2a229915ec764c63dd7
Semyon Stanovov <sestanovov@gmail.com>
2021-10-29 21:13:11 +0300

    add Search and SearchFiles. They make it easier to search the system

---
+++ src/main/java/ru/job4j/zip/Search.java
@@ -0,0 +1,15 @@
+package ru.job4j.zip;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
